6180c950c1d9c38e6378f474a0bfdff0a005ea02
Vladimir Martinenko <teslabest87@gmail.com>
2024-04-10 21:33:11 +0200

    add page

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Slider from '../../components/Slider/Slider';
+import Catalog from '../../components/Catalog/Catalog';
+import style from './MainPage.module.scss'
+import Footer from '../../components/Footer/Footer';
+import Product from '../../components/Product/Product';
+import Shares from '../../components/Shares/Shares';
+import Comment from '../../components/Сomment/Comment';
 
 const MainPage = () => {
   return (
-    <div>
+    <div className={style.main}>
       <Header/>
       <Slider/>
+      <Catalog/>
+      <Product/>
+      <Shares/>
+      <Comment/>
+      <Footer/>
     </div>
   );
 }
